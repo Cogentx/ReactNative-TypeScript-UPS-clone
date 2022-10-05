cd273a2f456c5880b734373e513fbc7aa13d04af
Cogentx <code@cogentx.com>
2022-10-05 15:15:31 -0230

    Setup Apollo GraphQL Client in app

--- App.tsx
+++ App.tsx
@@ -4,14 +4,24 @@ import { StyleSheet } from 'react-native';
 import { TailwindProvider } from 'tailwind-rn';
 import RootNavigator from './navigator/RootNavigator';
 import utilities from './tailwind.json';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
+// use stepzen api created in './stepzen'
+const client = new ApolloClient({
+  uri: 'http://localhost:5001/api/dev-zen',
+  cache: new InMemoryCache(),
+});
+
+// Wrap App in Higher Level Components
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
   );
 }
